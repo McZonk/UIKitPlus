ae86e5062dc733e31dd77559561a7da7463471d9
Max <mczonk@gmail.com>
2014-02-20 20:38:11 +0100

    Added public declarations

--- UIActionSheet+UIPIdentifier.h
+++ UIActionSheet+UIPIdentifier.h
@@ -2,4 +2,8 @@
 
 @interface UIActionSheet (UIPIdentifier)
 
+- (NSInteger)addButtonWithTitle:(NSString *)title identifier:(NSString *)identifier;
+
+- (NSString *)identifierForButtonAtIndex:(NSInteger)index;
+
 @end
